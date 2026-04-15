2fde9478bb1255dec99bd7fa7f6c84d478bf5f69
Akhil Singh <akhilkumarsingh428@gmail.com>
2026-04-15 21:19:31 +0530

    middleware file added for protected route

---
+++ src/middleware.tsx
@@ -0,0 +1,25 @@
+import { NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
+import type { NextRequest } from "next/server";
+
+export async function middleware(request: NextRequest) {
+  const token = await getToken({ req: request });
+  const url = request.nextUrl;
+
+  if (
+    token &&
+    (url.pathname.startsWith("/sign-in") || url.pathname.startsWith("/sign-up"))
+  ) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (!token && url.pathname.startsWith("/dashboard")) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/dashboard/:path*", "/sign-in", "sign-up"],
+};
